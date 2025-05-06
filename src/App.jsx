00cf013e3e8ecfd20acd5358f987bea4008bc2d6
Barbara Medina <BarbaraM@mckinstry.com>
2025-05-05 21:46:46 -0700

    week-07 parts 1 and 2 complete at this point.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,52 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 import TodoList from './features/TodoList/TodoList';
 import TodoForm from './features/TodoForm';
 
 function App() {
   const [todoList, setTodoList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+  const url = `https://api.airtable.com/v0/${import.meta.env.VITE_BASE_ID}/${import.meta.env.VITE_TABLE_NAME}`;
+  const token = `Bearer ${import.meta.env.VITE_PAT}`;
+
+  useEffect(() => {
+    const fetchTodos = async () => {
+      setIsLoading(true);
+      //define an object with fetched data
+      const options = {
+        method: 'GET',
+        headers: {
+          Authorization: token,
+        },
+      };
+      try {
+        //simulate fetching data from an API
+        const response = await fetch(url, options); //fetch line
+        if (!response.ok) {
+          throw new Error(`Error ${response.status}: ${response.statusText}`);
+        }
+
+        const result = await response.json();
+        const todos = result.records.map((record) => {
+          const todo = {
+            id: record.id,
+            title: fields.title,
+            isCompleted: fields.isCompleted ? true : false,
+          };
+          return todo;
+        });
+
+        setTodoList(todos);
+      } catch (error) {
+        setErrorMessage(error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    //call the async function
+    fetchTodos();
+  }, []);
 
   function handleAddTodo(newTodo) {
     const newTodoID = {
@@ -36,7 +78,15 @@ function App() {
         todoList={todoList}
         onCompleteTodo={completeTodo}
         onUpdateTodo={updateTodo}
-      />
+        isLoading={isLoading}
+      />{' '}
+      {errorMessage && (
+        <div>
+          <hr />
+          <p>{errorMessage}</p>
+          <button onClick={() => setErrorMessage('')}>Dismiss</button>
+        </div>
+      )}
     </div>
   );
 }
